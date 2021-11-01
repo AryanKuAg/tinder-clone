2b68e90a1a87ad9b8dac454440b3aeb6318f55e6
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-11-01 22:27:21 +0530

    seemscompleted

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:"http://localhost:7000"
+    baseURL:"https://alemantrix-tinder-backend.herokuapp.com"
 })
 
 export default instance
